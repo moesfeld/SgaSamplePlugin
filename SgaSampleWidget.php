97f5d741e073d288cb964ddc5936aa0deb46cfec
moesfeld <moesfeld@users.noreply.github.com>
2017-02-18 19:13:38 +0100

    correct spelling errors

--- SgaSampleWidget.php
+++ SgaSampleWidget.php
@@ -3,7 +3,7 @@
  * Sample widget for shopware shopping worlds
  * Copyright © 2016 Sebastian Garske 
  *
- * This file is part of Splash Widget.
+ * This file is part of Sample widget.
  *
  *  Sample widget is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Parallax Widget.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Sample widget.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace SgaSampleWidget;
